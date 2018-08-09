7fe3dfc480a5493b8545d6a1426511046c80e4dd
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-08-09 13:54:42 -0700

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# DC/OS Public Load Balancer Firewall
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|:----:|:-----:|:-----:|
+| cluster_name | Cluster Name | string | - | yes |
+| dcos_role | DCOS Role | string | - | yes |
+| instances_self_link | Instances Self Link | string | - | yes |
+| ip_addresss | Instance Type | string | - | yes |
+| network | Network Name | string | - | yes |
+| target | Target Self Link | string | - | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| cluster_name | Cluster Name |
+| dcos_role | DCOS Role |
+| instances_self_link | Instances Self Link |
+| ip_addresss | Instance Type |
+| network | Network Name |
+| target | Target Self Link |
---
+++ firewall.tf
@@ -0,0 +1,57 @@
+provider "google" {}
+
+# Reserving the Public IP Address of the External Load Balancer for the node
+resource "google_compute_address" "node" {
+  name = "${var.cluster_name}-external-${var.dcos_node}-node-address"
+}
+
+resource "google_compute_firewall" "allow-health-checks" {
+  name    = "${var.cluster_name}-${var.dcos_node}-allow-health-checks"
+  network = "${var.network}"
+
+  allow {
+    protocol = "tcp"
+  }
+
+  # The health check probes to your load balanced instances come from addresses in range 130.211.0.0/22 and 35.191.0.0/16.
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+}
+
+resource "google_compute_forwarding_rule" "external-node-forwarding-rule-http" {
+  name                  = "${var.cluster_name}-${var.dcos_role}-external-lb-forwarding-rule-http"
+  load_balancing_scheme = "EXTERNAL"
+  target                = "${var.target}"
+  port_range            = "80"
+  ip_address            = "${google_compute_address.node.address}"
+  depends_on            = ["google_compute_http_health_check.node-adminrouter-healthcheck"]
+}
+
+resource "google_compute_forwarding_rule" "external-node-forwarding-rule-https" {
+  name                  = "${var.cluster_name}-${var.dcos_role}-external-lb-forwarding-rule-https"
+  load_balancing_scheme = "EXTERNAL"
+  target                = "${var.target}"
+  port_range            = "443"
+  ip_address            = "${google_compute_address.node.address}"
+  depends_on            = ["google_compute_http_health_check.node-adminrouter-healthcheck"]
+}
+
+# Target Pool for external load balancing access
+resource "google_compute_target_pool" "node-pool" {
+  name = "${var.cluster_name}-${var.dcos_role}-pool"
+
+  instances = ["${var.instances_self_link}"]
+
+  health_checks = [
+    "${google_compute_http_health_check.node-adminrouter-healthcheck.name}",
+  ]
+}
+
+# Used for the external load balancer. The external load balancer only supports google_compute_http_health_check resource.
+resource "google_compute_http_health_check" "node-adminrouter-healthcheck" {
+  name                = "${var.cluster_name}-external-mesos-http-${var.dcos_role}-healthcheck"
+  check_interval_sec  = 30
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 2
+  port                = "80"
+}
