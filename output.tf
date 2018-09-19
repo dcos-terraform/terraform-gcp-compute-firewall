f72dcc376191f707d7ec59ddc9d1dd2d66db15d1
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-19 02:58:15 -0700

    init

--- output.tf
+++ output.tf
@@ -8,16 +8,6 @@ output "network" {
   value = "${var.network}"
 }
 
-# Instances Self Link
-output "instances_self_link" {
-  value = "${var.instances_self_link}"
-}
-
-# DCOS Role
-output "dcos_role" {
-  value = "${var.dcos_role}"
-}
-
 # List of allowed IPs to access DCOS cluster
 output "admin_cidr" {
   value = "${var.admin_cidr}"
--- variables.tf
+++ variables.tf
@@ -4,14 +4,6 @@ variable "name_prefix" {}
 # Network Name
 variable "network" {}
 
-# Instances Self Link
-variable "instances_self_link" {
-  type = "list"
-}
-
-# DCOS Role
-variable "dcos_role" {}
-
 # List of allowed IPs to access DCOS cluster
 variable "admin_cidr" {
   default = "0.0.0.0/0"
