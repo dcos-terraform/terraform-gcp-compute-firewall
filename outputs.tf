6500515402ae02119eef047bbd7dff0b17ef8c3b
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-27 14:20:38 -0700

    terraform fmt after description add

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 # Cluster Name
 output "name_prefix" {
   description = "Cluster Name"
-  value = "${var.name_prefix}"
+  value       = "${var.name_prefix}"
 }
 
 # Network Name
 output "network" {
   description = "network"
-  value = "${var.network}"
+  value       = "${var.network}"
 }
